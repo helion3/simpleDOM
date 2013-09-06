e95fa221270b6fb584319a158c2792d91ab9a591
nasonfish <nasonfish@gmail.com>
2013-09-06 16:45:14 -0600

    Added a few new functions to simpleDOM, similar to jQuery features.

--- simpledom.js
+++ simpledom.js
@@ -121,10 +121,30 @@ simpleDOM = function( selector ){
          * @returns simpleDOM
          */
         css: function( att, val ){
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                _matches[n].style[att] = val;
+            if(val){
+                for( var n = 0, l = _matches.length; n < l; n++ ){
+                    _matches[n].style[att] = val;
+                }
+                return this;
+            } else {
+                return this.get(0).style[att]; // does this work?
             }
-            return this;
+        },
+
+        /**
+         * Get the inner HTML of an object, or, with the optional
+         * html argument specified, sets the inner HTML of an object.
+         * @returns innerHTML
+         */
+        html: function(html){
+            if(!html){
+                return this.get(0).innerHTML;
+            } else {
+                for( var n = 0, l = _matches.length; n < l; n++){
+                    _matches[n].innerHTML = html;
+                }
+                return this;
+            }
         },
 
 
@@ -198,10 +218,84 @@ simpleDOM = function( selector ){
          */
         get: function(k){
             return _matches[k];
+        },
+
+        /**
+         * Get the value of an object, or, with the optional
+         * value argument, set the value.
+         * @returns simpleDOM | string
+         */
+        val: function(value){
+            if(!value){
+                return this.get(0).value;
+            } else {
+                for( var n = 0, l = _matches.length; n < l; n++){
+                    _matches[n].value = value;
+                }
+                return this;
+            }
+        },
+
+        /**
+         * Loop through all found elements and execute a function
+         * for each element.
+         * @param func
+         * @returns simpleDOM
+         */
+        each: function(func){
+            for( var n = 0, l = _matches.length; n < l; n++ ){
+                func(_matches[n]);
+            }
+            return this;
+        },
+
+        /**
+         * Remove elements from the page.
+         * @returns simpleDOM
+         */
+        remove: function(){
+            for (var n = 0, l = _matches.length; n < l; n++){
+                _matches[n].parentNode.removeChild(_matches[n]);
+            }
+            return this;
+        },
+
+        /**
+         * Insert HTML after a certain element
+         * @param html HTML in text format to insert
+         * @returns simpleDOM
+         */
+        after: function(html){
+            var element = document.createElement('div');
+            element.innerHTML = html;
+            for(var n = 0, l = _matches.length; n < l; n++){
+                var parent = _matches[n].parentNode;
+                if (parent.lastChild == _matches[n]) {
+                    parent.appendChild(element.firstChild);
+                } else {
+                    parent.insertBefore(element.firstChild, _matches[n].nextSibling);
+                }
+            }
+            return this;
+        },
+
+        /**
+         * Add HTML at the end of the inside of
+         * a certain element (append it)
+         * @param html HTML to append
+         * @returns simpleDOM
+         */
+        append: function(html){
+            // This is sort of hacky. not sure how else to do this.
+            var element = document.createElement('div');
+            element.innerHTML = html;
+            for(var n = 0, l = _matches.length; n < l; n++){
+                _matches[n].appendChild(element.firstChild);
+            }
+            return this;
         }
     }
-}
-
+};
 
 // function stopPropagation( e ){
 //     if (!e) var e = window.event;
