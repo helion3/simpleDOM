07bb79e6c466cdd8be380d27afd8fc89e387f3f0
Mike Botsko <botsko@gmail.com>
2014-02-07 23:14:55 -0800

    Rewriting encapsulation, cleaning up formatting, adding a few new method/tweaks

--- simpledom.js
+++ simpledom.js
@@ -3,208 +3,242 @@
  *
  * @author Mike Botsko
  *
- * Contains methods useful for this project, built to mimic
- * jQuery - developers who are familiar with jQuery will
+ * Contains methods useful for basic DOM manipulation, built to 
+ * mimic jQuery - developers who are familiar with jQuery will
  * have no trouble adapting to this.
  *
  * Works with all browser versions of the past few years.
  */
-simpleDOM = function( selector ){
-
-    var _raw_selector = selector
+(function (window,undefined){
+  
+  // Register function
+  window.simpleDOM = function(selector){
+    return new simpleDOMEngine(selector);
+  }
+  
+  var simpleDOMEngine = function(selector){
+    
+    this._raw_selector = selector;
 
     var _matches = [];
     if( typeof selector === "object" ){
         _matches = [selector];
     }
-    if( typeof selector === "string" ){
-        _matches = document.querySelectorAll(_raw_selector);
+    else if( typeof selector === "function" ){
+      this.ready(selector);
     }
+    else if( typeof selector === "string" ){
+        _matches = document.querySelectorAll(this._raw_selector);
+    }
+    
+    this.length = _matches.length;
+    for (var i = 0; i < _matches.length; i++){
+        this[i] = _matches[i];
+    }
+    
+    return this;
+    
+  }
+  
+  simpleDOMEngine.prototype = {
 
-    return {
+    // store selector
+    _raw_selector: false,
+    
 
-
-        /**
-         * Adds a new CSS class to each element
-         *
-         * @param className
-         * @returns simpleDOM
-         */
-        addClass: function( className ){
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                _matches[n].className += " " + className;
-            }
-            return this;
-        },
-
-
-        /**
-         * Removes a CSS class name from an element
-         * @param className
-         * @returns simpleDOM
-         */
-        removeClass: function( className ){
-            var regex = new RegExp('(?:^|\\s)'+className+'(?!\\S)');
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                _matches[n].className = _matches[n].className.replace( regex , '' );
-            }
-            return this;
-        },
-
-        /**
-         * Determine if an element has a CSS class name.
-         *
-         * @todo Only works for the first element matched by
-         * the selector. Will not currently work on subsequent
-         * elements
-         *
-         * @param className
-         * @returns boolean
-         */
-        hasClass: function( className ){
-            var regex = new RegExp('(?:^|\\s)'+className+'(?!\\S)');
-            if( _matches.length >= 1 ){
-                return this.get(0).className.match( regex );
-            }
-            return false;
-        },
+    /**
+     * Returns a wrapper instance of a single
+     * element.
+     * @param k
+     * @returns {*}
+     */
+    get: function(k){
+        return new simpleDOMEngine(this[k]);
+    },
 
 
-        /**
-         * Binds eventHandler of eventType to all elements
-         * matching selector.
-         *
-         * @param eventType
-         * @param eventHandler
-         * @returns simpleDOM
-         */
-        bind: function( eventType, eventHandler ){
-
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                var eventElem = _matches[n];
-                if(eventElem.addEventListener){
-                    eventElem.addEventListener(eventType, eventHandler, false);
-                } else {
-                    eventElem.attachEvent('on'+eventType, eventHandler);
-                }
-            }
-            return this;
-        },
+    /**
+     * Iterates every matching element and applies
+     * a function
+     * @param  {[type]} closure [description]
+     * @return {[type]}         [description]
+     */
+    each: function( closure ){
+      if( typeof closure !== "function" ) return false;
+      for( var n = 0, l = this.length; n < l; n++ ){
+        closure(n,this[n]);
+      }
+      return this;
+    },
 
 
-        /**
-         * Unbinds an event from all elements matching selector.
-         *
-         * @param eventType
-         * @param eventHandler
-         * @returns {*}
-         */
-        unbind: function( eventType, eventHandler ){
-
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                var eventElem = _matches[n];
-                if(eventElem.removeEventListener){
-                    eventElem.removeEventListener(eventType, eventHandler, false);
-                } else {
-                    eventElem.detachEvent('on'+eventType, eventHandler);
-                }
-            }
-            return this;
-        },
-
-
-        /**
-         * Set css attribute values
-         *
-         * @param att
-         * @param val
-         * @returns simpleDOM
-         */
-        css: function( att, val ){
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                _matches[n].style[att] = val;
-            }
-            return this;
-        },
-
-
-        /**
-         * Short-cut for hiding an element
-         *
-         * @returns simpleDOM
-         */
-        hide: function(){
-            this.css('display', 'none');
-            return this;
-        },
-
-
-        /**
-         * Short-cut for showing a hidden element
-         *
-         * @returns simpleDOM
-         */
-        show: function(){
-            this.css('display', 'block');
-            return this;
-        },
-
-
-        /**
-         * Add a new HTML attribute attr with value val
-         *
-         * @param attr
-         * @param val
-         * @returns simpleDOM
-         */
-        addAttr: function( attr, val ){
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                var newAttr = document.createAttribute(attr);
-                newAttr.value = val;
-                _matches[n].setAttributeNode(newAttr)
-            }
-            return this;
-        },
-
-
-        /**
-         * Returns the current value of an HTML attribute
-         *
-         * @param attr
-         * @returns {string}
-         */
-        attr: function( attr ){
-            return this.get(0).getAttribute(attr);
-        },
-
-
-        /**
-         * Removes an HTML attribute
-         * @param attr
-         * @returns simpleDOM
-         */
-        removeAttr: function( attr ){
-            for( var n = 0, l = _matches.length; n < l; n++ ){
-                _matches[n].removeAttribute(attr)
-            }
-            return this;
-        },
-
-
-        /**
-         * Returns the direct element
-         * @param k
-         * @returns {*}
-         */
-        get: function(k){
-            return _matches[k];
+    /**
+     * Execute function when the document has loaded
+     * @param  {[type]} closure [description]
+     * @return {[type]}         [description]
+     */
+    ready: function( closure ){
+      if( typeof closure !== "function" ) return;
+      window.document.onreadystatechange = function(){
+        if (window.document.readyState == "complete" || window.document.readyState == "loaded"){
+          closure();
         }
+      }
+    },
+
+
+    /**
+     * Adds a new CSS class to each element
+     *
+     * @param className
+     * @returns simpleDOM
+     */
+    addClass: function( className ){
+      for( var n = 0, l = this.length; n < l; n++ ){
+        // @todo fix spaces
+        this[n].className += " " + className;
+      }
+      return this;
+    },
+    /**
+     * Removes a CSS class name from an element
+     * @param className
+     * @returns simpleDOM
+     */
+    removeClass: function( className ){
+      var regex = new RegExp('(?:^|\\s)'+className+'(?!\\S)');
+      for( var n = 0, l = this.length; n < l; n++ ){
+        this[n].className = this[n].className.replace( regex , '' );
+      }
+      return this;
+    },
+    /**
+     * Determine if an element has a CSS class name.
+     *
+     * @todo Only works for the first element matched by
+     * the selector. Will not currently work on subsequent
+     * elements
+     *
+     * @param className
+     * @returns boolean
+     */
+    hasClass: function( className ){
+      var regex = new RegExp('(?:^|\\s)'+className+'(?!\\S)');
+      if( this.length >= 1 ){
+        return this[0].className.match( regex ) || false;
+      }
+      return false;
+    },
+
+
+    /**
+     * Binds eventHandler of eventType to all elements
+     * matching selector.
+     *
+     * @param eventType
+     * @param eventHandler
+     * @returns simpleDOM
+     */
+    bind: function( eventType, eventHandler ){
+
+      for( var n = 0, l = this.length; n < l; n++ ){
+        var eventElem = this[n];
+        if(eventElem.addEventListener){
+          eventElem.addEventListener(eventType, eventHandler, false);
+        } else {
+          eventElem.attachEvent('on'+eventType, eventHandler);
+        }
+      }
+      return this;
+    },
+    /**
+     * Unbinds an event from all elements matching selector.
+     *
+     * @param eventType
+     * @param eventHandler
+     * @returns {*}
+     */
+    unbind: function( eventType, eventHandler ){
+
+      for( var n = 0, l = this.length; n < l; n++ ){
+        var eventElem = this[n];
+        if(eventElem.removeEventListener){
+          eventElem.removeEventListener(eventType, eventHandler, false);
+        } else {
+          eventElem.detachEvent('on'+eventType, eventHandler);
+        }
+      }
+      return this;
+    },
+
+
+    /**
+     * Set css attribute values
+     *
+     * @param att
+     * @param val
+     * @returns simpleDOM
+     */
+    css: function( att, val ){
+      for( var n = 0, l = this.length; n < l; n++ ){
+        this[n].style[att] = val;
+      }
+      return this;
+    },
+    /**
+     * Short-cut for hiding an element
+     *
+     * @returns simpleDOM
+     */
+    hide: function(){
+      this.css('display', 'none');
+      return this;
+    },
+    /**
+     * Short-cut for showing a hidden element
+     *
+     * @returns simpleDOM
+     */
+    show: function(){
+      this.css('display', 'block');
+      return this;
+    },
+
+
+    /**
+     * Add a new HTML attribute attr with value val
+     *
+     * @param attr
+     * @param val
+     * @returns simpleDOM
+     */
+    addAttr: function( attr, val ){
+        for( var n = 0, l = this.length; n < l; n++ ){
+            var newAttr = document.createAttribute(attr);
+            newAttr.value = val;
+            this[n].setAttributeNode(newAttr)
+        }
+        return this;
+    },
+    /**
+     * Returns the current value of an HTML attribute
+     *
+     * @param attr
+     * @returns {string}
+     */
+    attr: function( attr ){
+        return this.get(0).getAttribute(attr);
+    },
+    /**
+     * Removes an HTML attribute
+     * @param attr
+     * @returns simpleDOM
+     */
+    removeAttr: function( attr ){
+        for( var n = 0, l = this.length; n < l; n++ ){
+            this[n].removeAttribute(attr)
+        }
+        return this;
     }
-}
-
-
-// function stopPropagation( e ){
-//     if (!e) var e = window.event;
-//     e.cancelBubble = true;
-//     if (e.stopPropagation) e.stopPropagation();
-// }
+  }
+})(this);
